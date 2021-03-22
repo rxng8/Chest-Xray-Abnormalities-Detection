3a692f9723f1bdc484d0966b5701df2aa43f2b6c
Alex Nguyen <alexvn.edu@gmail.com>
2021-03-23 00:33:12 +0700

    update notebook

--- faster_rnn.py
+++ faster_rnn.py
@@ -8,6 +8,7 @@ import re
 import pydicom
 import warnings
 
+import sys
 import time
 
 import matplotlib.pyplot as plt
@@ -43,10 +44,18 @@ from core.data import DatasetCOCO, DatasetCOCOPytorch, show_img
 
 paddingSize= 0
 
+IMG_SHAPE = (256, 256)
+BATCH_SIZE = 2
+
 DIR_INPUT = './dataset/vinbigdata-coco-dataset-with-wbf-3x-downscaled/vinbigdata-coco-dataset-with-wbf-3x-downscaled'
 DIR_TRAIN = f'{DIR_INPUT}/train'
 DIR_TEST = f'{DIR_INPUT}/test'
 
+MODEL_DIR = "./models/"
+
+root_data_folder = "./dataset/vinbigdata-coco-dataset-with-wbf-3x-downscaled/vinbigdata-coco-dataset-with-wbf-3x-downscaled"
+
+device = torch.device('cuda') if torch.cuda.is_available() else torch.device('cpu')
 
 # %%
 
@@ -64,48 +73,62 @@ device = torch.device('cuda') if torch.cuda.is_available() else torch.device('cp
 
 # %%
 
-IMG_SHAPE = (256, 256)
+# ds = DatasetCOCO(root_data_folder, img_shape=IMG_SHAPE)
 
-root_data_folder = "./dataset/vinbigdata-coco-dataset-with-wbf-3x-downscaled/vinbigdata-coco-dataset-with-wbf-3x-downscaled"
-
-ds = DatasetCOCO(root_data_folder, img_shape=IMG_SHAPE)
+# len(ds.dataset["train"].values())
 
 # %%
 
-len(ds.dataset["train"].values())
+def dilation(img): # custom image processing function
+    kernel = cv2.getStructuringElement(cv2.MORPH_ELLIPSE, tuple(np.random.randint(1, 6, 2)))
+    img = cv2.dilate(img, kernel, iterations=1)
+    return img
 
-# %%
+class Dilation(ImageOnlyTransform):
+    def apply(self, img, **params):
+        return dilation(img)
+
+def collate_fn(batch):
+    return tuple(zip(*batch))
 
 def get_train_transform():
-    return A.Compose([
-        A.Flip(0.5),
-        A.ShiftScaleRotate(scale_limit=0.1, rotate_limit=45, p=0.25),
-        A.LongestMaxSize(max_size=800, p=1.0),
-        # Dilation(),
-        # FasterRCNN will normalize.
-        A.Normalize(mean=(0, 0, 0), std=(1, 1, 1), max_pixel_value=255.0, p=1.0),
-        ToTensorV2(p=1.0)
-    ], bbox_params={'format': 'pascal_voc', 'label_fields': ['labels']})
+    # return A.Compose([
+    #     A.Flip(0.5),
+    #     A.ShiftScaleRotate(scale_limit=0.1, rotate_limit=45, p=0.25),
+    #     A.LongestMaxSize(max_size=800, p=1.0),
+    #     # Dilation(),
+    #     # FasterRCNN will normalize.
+    #     A.Normalize(mean=(0, 0, 0), std=(1, 1, 1), max_pixel_value=255.0, p=1.0),
+    #     ToTensorV2(p=1.0)
+    # ], bbox_params={'format': 'pascal_voc', 'label_fields': ['labels']})
+    return A.Compose(
+        [
+            # A.Flip(0.5),
+            # A.ShiftScaleRotate(scale_limit=0.1, rotate_limit=45, p=0.25),
+            # A.LongestMaxSize(max_size=800, p=1.0),
+            # Dilation(),
+            A.Normalize(mean=(0, 0, 0), std=(1, 1, 1), max_pixel_value=255.0, p=1.0),
+            ToTensorV2(p=1.0)
+        ],
+        bbox_params={'format': 'pascal_voc', 'label_fields': ['labels']}
+    )
 
 
 # train_dataset = DatasetCOCOPytorch(root=root_data_folder, img_shape=IMG_SHAPE, transform=get_train_transform())
 
 # %%
 
-device = torch.device('cuda') if torch.cuda.is_available() else torch.device('cpu')
-
-def collate_fn(batch):
-    return tuple(zip(*batch))
 train_dataset = DatasetCOCOPytorch(root=root_data_folder, img_shape=IMG_SHAPE, transforms=get_train_transform())
 # train_dataset = DatasetCOCOPytorch(train_df, DIR_TRAIN, get_train_transform())
 valid_dataset = DatasetCOCOPytorch(root=root_data_folder, img_shape=IMG_SHAPE, train_set=False, transforms=get_train_transform())
 
 # split the dataset in train and test set
-indices = torch.randperm(len(train_dataset)).tolist()
+# indices = torch.randperm(len(train_dataset)).tolist()
+
 # Create train and validate data loader
 train_data_loader = DataLoader(
     train_dataset,
-    batch_size=4,
+    batch_size=BATCH_SIZE,
     shuffle=True,
     # num_workers=4,
     collate_fn=collate_fn
@@ -113,7 +136,7 @@ train_data_loader = DataLoader(
 
 valid_data_loader = DataLoader(
     valid_dataset,
-    batch_size=4,
+    batch_size=BATCH_SIZE,
     shuffle=False,
     # num_workers=4,
     collate_fn=collate_fn
@@ -128,7 +151,6 @@ images, targets, image_ids = next(iter(train_data_loader))
 
 # %%
 
-
 images, targets, image_ids = train_dataset[2]
 # %%
 
@@ -136,29 +158,7 @@ images.shape
 
 # %%
 
-targets 
-
-# %%
-
-# Train dataset sample
-images, targets, image_ids = next(iter(train_data_loader))
-images = list(image.to(device) for image in images)
-targets = [{k: v.to(device) for k, v in t.items()} for t in targets]
-
-for number in random.sample([1,2,3],3):
-  boxes = targets[number]['boxes'].cpu().numpy().astype(np.int32)
-  img = images[number].permute(1,2,0).cpu().numpy()
-  labels= targets[number]['labels'].cpu().numpy().astype(np.int32)
-  fig, ax = plt.subplots(1, 1, figsize=(16, 8))
-
-  for i in range(len(boxes)):
-      img = cv2.rectangle(img,(boxes[i][0]+paddingSize,boxes[i][1]+paddingSize),(boxes[i][2]+paddingSize,boxes[i][3]+paddingSize),(255,0,0),2)
-      #print(le.inverse_transform([labels[i]-1])[0])
-      #print(label_to_name(labels[i]), (int(boxes[i][0]), int(boxes[i][1])))
-      img = cv2.putText(img, label_to_name(labels[i]), (int(boxes[i][0]), int(boxes[i][1])), cv2.FONT_HERSHEY_TRIPLEX,1, (255,0,0), 2, cv2.LINE_AA)
-
-  ax.set_axis_off()
-  ax.imshow(img)
+targets
 
 
 # %%
@@ -175,32 +175,49 @@ num_epochs =  1 #Low epoch to save GPU time
 itr = 1
 for epoch in range(num_epochs):
     # loss_hist.reset()
+    print("Epochs:", epoch)
+    time.sleep(1)
     with tqdm(total=len(train_dataset)) as pbar:
         for images, targets, image_ids in train_data_loader:
-            
-            images = list(image.to(device) for image in images)
-            targets = [{k: v.to(device) for k, v in t.items()} for t in targets]
+            try:
+                images = list(image.to(device) for image in images)
+                targets = [{k: v.to(device) for k, v in t.items()} for t in targets]
 
-            loss_dict = model(images, targets)  
-            
-            losses = sum(loss for loss in loss_dict.values())
-            loss_value = losses.item()
+                loss_dict = model(images, targets)  
+                
+                losses = sum(loss for loss in loss_dict.values())
+                loss_value = losses.item()
 
-            optimizer.zero_grad()
-            losses.backward()
-            optimizer.step()
+                optimizer.zero_grad()
+                losses.backward()
+                optimizer.step()
 
-            if itr % 50 == 0:
-                print(f"Iteration #{itr} loss: {loss_value}")
+                if itr % 50 == 0:
+                    print(f"Iteration #{itr} loss: {loss_value}")
 
-            itr += 1
-            pbar.update(1)
-    
+                itr += 1
+                pbar.update(1)
+            except:
+                continue
+            # break
+        # pass
     # update the learning rate
     # if lr_scheduler is not None:
     #     lr_scheduler.step()
     # lossHistoryepoch.append(loss_hist.value)
     # print(f"Epoch #{epoch} loss: {loss_hist.value}")  
+    # pass
+
+# %%
+MODEL_NAME = "model1.pth"
+torch.save(model.state_dict(), os.path.join(MODEL_DIR, MODEL_NAME))
 
 
 # %%
+
+
+# Eval
+
+
+
+
